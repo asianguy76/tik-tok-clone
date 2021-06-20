f963ac29544701a087e8ee20d4519a47115de91c
asianguy76 <takecareofknives@gmail.com>
2021-06-19 21:06:19 -0500

    Added Components. Some CSS. Removed more boilerplate

--- tik-tok-clone/src/index.js
+++ tik-tok-clone/src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Upload from './pages/Upload';
+import Header from './components/Header';
+import './App.css';
+
+
+function App() {
+  return (
+    <HashRouter>
+      <Header />
+      <Switch>
+      <Route path='/upload' component={Upload}/>
+      <Route path='/' component={Home}/>
+        
+      </Switch>
+    </HashRouter>
+  );
+}
+
+export default App;
 
 ReactDOM.render(
   <React.StrictMode>
